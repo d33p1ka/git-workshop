56b15655fa26dd4cfe3956dde5b2ea43a449d5e3
d33p1ka <rdeepsm@yahoo.co.in>
2012-05-26 15:18:58 +0800

    Update test/relativeclock_spec.rb

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -5,8 +5,16 @@ describe SingaporeRelativeClock do
 		@clock = SingaporeRelativeClock.new
 	end
 
-	it "should..." do
-
+	it "should return zero for any city in the same timezone as Singapore" do
+		@clock.timeDifferenceTo("Singapore").should === 0
+	end
+	
+	it "should raise an error if no data is entered"
+	
+	end
+	
+	it "should return -2 for Sydney" do
+	 @clock.timeDifferenceTo("Sydney").should === -2
 	end
 
 end
