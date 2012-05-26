cbfe7404b1f37fd8c3294b1849034ff0d57a3d17
d33p1ka <rdeepsm@yahoo.co.in>
2012-05-26 16:14:54 +0800

    Update lib/singapore_relative_clock.rb

--- lib/singapore_relative_clock.rb
+++ lib/singapore_relative_clock.rb
@@ -1,12 +1,13 @@
 require_relative 'gmt_offset_finder'
 
+@finder = GMTOffsetFinder.new
+	singapore_time = @finder.offsetFor("Singapore")
 class SingaporeRelativeClock
 
-	@finder = GMTOffsetFinder.new
-	singapore_time = @finder.offsetFor("Singapore")
+	
 	def timeDifferenceTo(city)
 	 city_time = @finder.offsetFor(city)
-	 singapore_time - city_time
+	  return singapore_time - city_time
 	 #if city == "Singapore"
 	 #	return 0
 	 #end
